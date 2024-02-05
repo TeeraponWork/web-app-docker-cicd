3646cf5f15b77d335781dee396ccbb1555fc98b5
Teerapon Boonthong <55040066+TeeraponWork@users.noreply.github.com>
2024-02-05 08:25:27 +0700

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "TestFreezing", "TestBracing", "TestChilly", "TestCool", "TestMild", "TestWarm", "TestBalmy", "TestHot", "TestSweltering", "TestScorching"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
